5f998ca0a401060128ae5ea71b13cfa464b865a7
nellymilk <nellyshie@gmail.com>
2017-05-24 17:33:16 +0800

    test

--- app.py
+++ app.py
@@ -35,21 +35,23 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-    text = str(event.message.text)+' OK!' #message from user
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=text)) #reply the same message from user
+    if event.message.type == 'text':
+        text = str(event.message.text)+' OK!' #message from user
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=text)) #reply the same message from user
 
 
-@handler.add(MessageEvent, message=LocationMessage)
-def handle_location_message(event):
-    #text = str(event.message.text)+' OK!' #message from user
+# @handler.add(MessageEvent, message=LocationMessage)
+# def handle_location_message(event):
+#     #text = str(event.message.text)+' OK!' #message from user
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=str(event.message.latitude)+','+str(event.message.longitude))
-    ) #reply the same message from user    
+#     line_bot_api.reply_message(
+#         event.reply_token,
+#         TextSendMessage(text=str(event.message.latitude)+','+str(event.message.longitude))
+#     ) #reply the same message from user    
     
 
 import os
