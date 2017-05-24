da0f42ad41f0a76ec0a84f745edd52ba5a368447
nellymilk <nellyshie@gmail.com>
2017-05-24 11:19:26 +0800

    test reply message

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-    text = ' OK!'+ str(event.message.text) #message from user
+    text = str(event.message.text)+' OK!' #message from user
 
     line_bot_api.reply_message(
         event.reply_token,
