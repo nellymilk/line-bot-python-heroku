070071accfb9ad6e3711d0457e6fb1f4e212ef54
nellymilk <nellyshie@gmail.com>
2017-05-25 16:43:50 +0800

    test

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def handle_text_message(event):
     if event.message.type == 'text':
         tz = pytz.timezone('Asia/Taipei')
         #time_now = time.strftime(' %Y-%m-%d %H:%M:%S %Z', time.gmtime(event.timestamp/1000)).replace(tzinfo=pytz.timezone('UTC'))
-        time_now = datetime.fromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S').replace(tzinfo=pytz.utc)
+        time_now = datetime.fromtimestamp(event.timestamp).strftime('%Y-%m-%d %H:%M:%S').replace(tzinfo=pytz.utc)
         #.replace(tzinfo=pytz.utc)
         time_tw = time_now.astimezone(tz)
 
