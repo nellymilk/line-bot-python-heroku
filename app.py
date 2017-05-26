ead8a12fecd5690778940e806e102405e2286fae
nellymilk <nellyshie@gmail.com>
2017-05-26 12:55:49 +0800

    test

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def handle_text_message(event):
         time_tw = time_now.astimezone(tz)
         time = time_tw.strftime(' %Y-%m-%d %H:%M:%S')
 
-        print event.source['userId']
+        print event.source.userId
 
         text = event.message.text + time + ' ID: \n' #message from user
 
