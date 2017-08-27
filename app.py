7da52ddbc7f4b3a641b4befcc2c458d20dcff0e8
nelly <nelly@nellys-MacBook-Pro.local>
2017-08-27 17:02:32 +0800

    add industry in help output

--- app.py
+++ app.py
@@ -132,6 +132,7 @@ def find_Industry(url):
         'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
     }    
     response = requests.get(url,headers=header)
+    print('response '+str(response))
     html = response.content.decode('utf-8')
 
     page = etree.HTML(html)
